b90a23fbb1a9dade32714d293c12dcca5b3918ce
Dan Zemlyansky <danzemly000@gmail.com>
2024-08-06 15:19:26 +0300

    city input mostly complete

--- fintek-weather-app/src/components/CityInput.tsx
+++ fintek-weather-app/src/components/CityInput.tsx
@@ -1,15 +1,22 @@
-import '../styles/CityInput.css'
+import "../styles/CityInput.css";
 
-export default function CityInput(){
-
-
-
-
-    return(
-        <div id="cityInputContainer">
-<label id="cityInputLabel" htmlFor="cityInput">City name</label>
-<input aria-label="City Input" name="cityInput" id="cityInput"></input>
-<button type="submit">Check</button>
-        </div>
-    )
+export default function CityInput() {
+  return (
+    <div className="cityInputContainer">
+      <label htmlFor="cityInput" className="cityInputLabel">
+        City name
+      </label>
+      <div className="inputButtonContainer">
+        <input
+          aria-label="City Input"
+          name="cityInput"
+          id="cityInput"
+          className="cityInput"
+        />
+        <button type="submit" className="cityInputButton">
+          Check
+        </button>
+      </div>
+    </div>
+  );
 }
