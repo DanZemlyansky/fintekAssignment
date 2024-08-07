465d1f5894a8b58bfa8d08d86360b2bdfdcd4164
Dan Zemlyansky <danzemly000@gmail.com>
2024-08-07 13:05:28 +0300

    card done

--- fintek-weather-app/src/components/card/WeatherCardHead.tsx
+++ fintek-weather-app/src/components/card/WeatherCardHead.tsx
@@ -1,11 +1,15 @@
+import '../../styles/WeatherCardHead.css'
+
 export default function WeatherCardHead(){
 
 
     return(
-        <>
-        <h1>City Name</h1>
-        <h2>Country</h2>
-        <h3>Date AT Hour</h3>
-        </>
+        <section id='weatherHeadContainer'>
+        <div id='weatherHeadLocation'>
+            <h1 id='cityName'>Tel Aviv</h1>
+            <h2 id='countryName'>Israel</h2>
+        </div>
+        <h3 id='weatherHeadDate'>13/2/2 at 16:00</h3>
+        </section>
     )
 }
