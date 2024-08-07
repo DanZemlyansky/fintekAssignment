3d97fa9dfa7f9a38acb5af31b7f47c036be8f64f
Dan Zemlyansky <danzemly000@gmail.com>
2024-08-07 14:18:45 +0300

    xd layout done

--- fintek-weather-app/src/components/containers/HeroContainer.tsx
+++ fintek-weather-app/src/components/containers/HeroContainer.tsx
@@ -1,17 +1,22 @@
 import CityInput from "../CityInput";
-import fintekLogo from '../../assets/logo.svg'
+import fintekLogo from "../../assets/logo.svg";
 import LatLong from "../hero/LatLong";
-import '../../styles/HeroContainer.css'
+import "../../styles/HeroContainer.css";
 
 //! pass function to cityInput and call the backend from there
-export default function HeroContainer(){
-
-    return(
-        <section id="heroContainer">
-            <img id="fintekImg" src={fintekLogo}  alt='fintek'></img>
-            <p id="heroText">Use our weather app to see the weather around the world</p>
-<CityInput/>
-<LatLong/>
-        </section>
-    )
+export default function HeroContainer() {
+  return (
+    <section id="heroContainer">
+      <div id="heroImgContainer">
+        <img id="fintekImg" src={fintekLogo} alt="fintek"></img>
+      </div>
+      <div className="heroMainContainer">
+          <p id="heroText">
+            Use our weather app to see the weather around the world
+          </p>
+          <CityInput />
+      </div>
+      <LatLong />
+    </section>
+  );
 }
--- fintek-weather-app/src/components/hero/LatLong.tsx
+++ fintek-weather-app/src/components/hero/LatLong.tsx
@@ -1,10 +1,14 @@
+import '../../styles/LatLong.css'
+
 export default function LatLong(){
 
 //call api on load to get current data
     return(
         <section id="latLongContainer">
-        <span className="latLong">latitude</span>
-        <span className="latLong">longitude</span>
+        <div id="latLongData">
+            <span className="latLong">latitude</span>
+            <span className="latLong">longitude</span>
+        </div>
         <span className="latLong">accurate to ... at ...</span>
         </section>
     )
